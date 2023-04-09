9343dff4eca2ceefa67057d8f90e7fef804f7f05
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-04-09 18:40:13 -0500

    updated schema, still getting errors

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,16 @@
 DROP DATABASE IF EXISTS aviatrix_blog_db;
 
 CREATE DATABASE aviatrix_blog_db;
+
+CREATE TABLE blogs (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  instagram VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+INSERT INTO blogs (title, description, body, instagram) VALUES 
